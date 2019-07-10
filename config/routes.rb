facc1a61bce6319cdda356d1edfc7364ae82d946
aidenjl193 <aidenjl193@gmail.com>
2019-07-10 11:13:19 +0100

    fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :offers
   scope '/api' do
     post 'user_token' => 'user_token#create'
     resources :items
     resources :users
+    resources :offers
     post '/items/:id/offer', to: 'items#create_offer'
   end
 end
