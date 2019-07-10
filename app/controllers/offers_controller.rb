e429be3c51bbee7ee6f67bdb858eca82d81ad6a6
aidenjl193 <aidenjl193@gmail.com>
2019-07-10 11:07:41 +0100

    Offers routes

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,8 @@
 class OffersController < ApplicationController
+
+  def show
+    offer = Offer.find(params[:id])
+    render json: offer.to_json(
+             :include => {:item_offers => {}})
+  end
 end
