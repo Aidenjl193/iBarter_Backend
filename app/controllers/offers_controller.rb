4448ba7c5e78aabe1b701b266704134e7819f7ef
aidenjl193 <aidenjl193@gmail.com>
2019-07-10 11:17:01 +0100

    fixes

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -3,6 +3,6 @@ class OffersController < ApplicationController
   def show
     offer = Offer.find(params[:id])
     render json: offer.to_json(
-             :include => {:item_offers => {}})
+             :include => {:items => {}})
   end
 end
